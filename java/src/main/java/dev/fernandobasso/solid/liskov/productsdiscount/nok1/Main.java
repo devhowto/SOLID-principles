ee986e96e5875b561876993658017b3b74682c9e
Fernando Basso <fernandobasso.br@gmail.com>
2022-02-05 09:53:21 -0300

    Add Liskov Substitution Principle NOK1 example
    OBS: NOK1 means “Non-OK Example 1”.

---
+++ java/src/main/java/dev/fernandobasso/solid/liskov/productsdiscount/nok1/Main.java
@@ -0,0 +1,41 @@
+package dev.fernandobasso.solid.liskov.productsdiscount.nok1;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+  public static void main (String[] args) {
+    //
+    // All three objects are of reference type Product. InHouseProduct is,
+    // however, a subtype of Product, and has a method that Product doesn't
+    // have.
+    //
+    Product p1 = new Product();
+    Product p2 = new Product();
+    Product p3 = new InHouseProduct();
+
+    List<Product> products = new ArrayList<Product>();
+
+    products.add(p1);
+    products.add(p2);
+    products.add(p3);
+
+    for (Product product : products) {
+      //
+      // Tell, don't ask.
+      //
+      // But here we are asking if product is an instance of InHouseProduct.
+      //
+      if (product instanceof InHouseProduct) {
+        ((InHouseProduct) product).applyExtraDiscount();
+      }
+
+      System.out.println(product.getDiscount());
+    }
+  }
+}
+
+//
+// We should not have to ask for the type of the product. We should
+// be able to deal with all products as a generic Product objects.
+//
